1efb8de080e2bf05773303f3d7148090ab55c082
DawonOh <dawonohh@gmail.com>
2022-10-20 23:13:02 +0900

    Main.js 필요없는 코드 삭제

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -3,23 +3,6 @@ import "./Main.scss";
 import Feed from "./Component/Feed";
 
 function Main() {
-  //댓글 목록 관리
-  const [commentArray, setCommentArray] = useState([]);
-
-  //각 댓글에는 고유한 key값이 있어야 한다.
-  const [id, setId] = useState(1);
-  const value = useRef();
-
-  //댓글 추가 함수
-  const addComment = () => {
-    setId(id + 1);
-    const newComment = {
-      id: id,
-      content: value.current.value,
-    };
-
-    setCommentArray([...commentArray, newComment]);
-  };
   return (
     <Fragment>
       <header>
@@ -46,108 +29,8 @@ function Main() {
       </header>
       <main className="alignCenter">
         <Feed />
+        {/* feed 2번 */}
         {/* <div className="feed">
-          <div className="feedHeader">
-            <div className="profileDiv alignCenter">
-              <div className="user alignCenter">
-                <img
-                  src={process.env.PUBLIC_URL + `/images/cookie.jpg`}
-                  alt="프로필사진"
-                  className="profileImg"
-                />
-                <span className="userName">dawon_Oh</span>
-              </div>
-              <div className="feedHeaderMenu">
-                <img
-                  src={process.env.PUBLIC_URL + `/images/option.png`}
-                  alt="피드메뉴"
-                  className="feedHeaderMenuImg"
-                />
-              </div>
-            </div>
-          </div>
-          <div className="feedImgDiv alignCenter">
-            <img
-              src={process.env.PUBLIC_URL + `/images/sky.jpg`}
-              alt="하늘이미지"
-              className="feedImg"
-            />
-          </div>
-          <div className="feedMenu">
-            <div id="feedLeft" className="alignCenter">
-              <div id="feedHeart">
-                <img
-                  src={process.env.PUBLIC_URL + `/images/heart.png`}
-                  alt="좋아요"
-                  className="feedIcon"
-                />
-              </div>
-              <div id="feedComment">
-                <img
-                  src={process.env.PUBLIC_URL + `/images/speech-bubble.png`}
-                  alt="댓글보기"
-                  className="feedIcon"
-                />
-              </div>
-              <div id="feedSend">
-                <img
-                  src={process.env.PUBLIC_URL + `/images/send.png`}
-                  alt="공유하기"
-                  className="feedIcon"
-                />
-              </div>
-            </div>
-            <div id="feedRight" className="alignCenter">
-              <div id="feedMark">
-                <img
-                  src={process.env.PUBLIC_URL + `/images/bookmark-white.png`}
-                  alt="저장하기"
-                  className="feedIcon"
-                />
-              </div>
-            </div>
-          </div>
-          <div className="comment">
-            <p className="countLikes commentP">좋아요 4개</p>
-            <p className="commentP">
-              <span className="writer">dawon_Oh</span>
-              <span className="content">날씨 좋다🌞</span>
-            </p>
-            {commentArray.map((comment) => {
-              return (
-                <p className="commentP" key={comment.id}>
-                  <span className="writer">dawon_Oh</span>
-                  <span className="tag">@friend_1</span>
-                  <span className="content">{comment.content}</span>
-                </p>
-              );
-            })}
-            <div className="writeDate grayFont">2일 전</div>
-          </div>
-          <div className="addComment alignCenter">
-            <div className="addCommentLogoAndInput alignCenter">
-              <div className="smileIcon">
-                <img
-                  src={process.env.PUBLIC_URL + `/images/emoticon.png`}
-                  alt="댓글창 로고"
-                  className="smile"
-                />
-              </div>
-              <div className="inputForm">
-                <input
-                  type="text"
-                  placeholder="댓글 달기..."
-                  className="commentInput"
-                  ref={value}
-                />
-              </div>
-            </div>
-            <button className="addBtn" onClick={addComment}>
-              게시
-            </button>
-          </div>
-        </div>
-        <div className="feed">
           <div className="feedHeader">
             <div className="profileDiv alignCenter">
               <div className="user alignCenter">
@@ -244,8 +127,9 @@ function Main() {
             </div>
             <button className="addBtn">게시</button>
           </div>
-        </div>
-        <div className="feed">
+        </div> */}
+        {/* feed 3번 */}
+        {/* <div className="feed">
           <div className="feedHeader">
             <div className="profileDiv alignCenter">
               <div className="user alignCenter">
